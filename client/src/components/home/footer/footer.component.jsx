2b8149a6c9f7ecb8caec8f5f825de0d82a1934f6
Sahanajayakumar <sahanajayakumar01@gmail.com>
2023-03-21 18:37:43 +0530

    added services and refined the footer

--- client/src/components/home/footer/footer.component.jsx
+++ client/src/components/home/footer/footer.component.jsx
@@ -1,6 +1,6 @@
 import { ReactComponent as LinkedInIcon } from '../../../assets/icons/linkedIn.svg';
-import { ReactComponent as Partner1 } from '../../../assets/home/partner-aws.svg';
-import { ReactComponent as Partner2 } from '../../../assets/home/partner-microsoft.svg';
+import Partner1 from '../../../assets/home/aws.jpg';
+import Partner2  from '../../../assets/home/microsoft.jpg';
 import { Link } from 'react-router-dom';
 
 import './footer.styles.scss';
@@ -9,7 +9,7 @@ const Footer = () => {
     return (
         <div className='footer'>
             <div className='footer__company-vision'>
-                <h2>Our Visoin</h2>
+                <h2>Our Vision</h2>
                 <p>
                     At GAC we want to help every small- and mid-sized firm leverage the power of cloud technology by
                     reducing their overall infrastructure cost and boosting their profits in order to help them achieve their goals.
@@ -18,8 +18,8 @@ const Footer = () => {
             <div className='footer__cta row'>
                 <div className='column'>
                     <h3>Partners</h3>
-                    <Partner1 className='icon'/>
-                    <Partner2 className='icon'/>
+                    <img src={Partner1} alt="awsimage" className="icon"/>
+                    <img src={Partner2} alt="microsoftimage" className="icon"/>
                 </div>
                 <div className='column'>
                     <h3>Office</h3>
@@ -29,11 +29,10 @@ const Footer = () => {
                 </div>
                 <div className='column'>
                     <h3>Links</h3>
-                    <p>Home</p>
-                    <p>Services</p>
+                    <p>Hub</p>
                     <p>
                         <Link to={'https://goagilecloud.com/'}>
-                            GoAgileCloud Home Page
+                            GoAgileCloud
                         </Link>
                     </p>
                 </div>
